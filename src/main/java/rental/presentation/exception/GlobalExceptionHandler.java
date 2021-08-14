9d8fc33aec89e33d24fd76ca37d63b87c2cb8d2d
qlyan <qlyan@thoughtworks.com>
2021-08-15 03:00:59 +0800

    return error response when catch not found exception

---
+++ src/main/java/rental/presentation/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package rental.presentation.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import rental.presentation.dto.response.common.ErrorResponse;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(NotFoundException exception) {
+        log.warn(exception.getMessage());
+        return ErrorResponse.of(HttpStatus.NOT_FOUND.value(), exception.getCode(), exception.getMessage());
+    }
+}
--- src/test/java/rental/presentation/controller/ETicketControllerTest.java
+++ src/test/java/rental/presentation/controller/ETicketControllerTest.java
@@ -13,11 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import rental.client.application.ETicketApplicationService;
 import rental.presentation.dto.command.CreateETicketCommand;
+import rental.presentation.exception.NotFoundException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -56,6 +60,30 @@ public class ETicketControllerTest {
         // then
         verify(applicationService, times(1)).create(eq(1L),
                 eq(flight), eq(userName), eq(userId));
+    }
+
+    @Test
+    public void should_throw_exception_when_validate_order_exist_fail() throws Exception {
+        // given
+        String flight = "CA123";
+        String userName = "user-name";
+        String userId = "user-id";
+        CreateETicketCommand command = CreateETicketCommand.builder().flight(flight)
+                                                           .userID(userId).userName(userName).build();
+
+        when(applicationService.create(any(), any(), any(), any()))
+                .thenThrow(new NotFoundException("NOT_FOUND_ORDER", String.format("order id: %d not found", 1)));
+
+        // when
+        mvc.perform(
+                post("/booking-orders/1/e-tickets")
+                        .content(objectMapper.writeValueAsString(command))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+           .andExpect(status().isNotFound())
+           .andExpect(jsonPath("$.code").value("NOT_FOUND_ORDER"));
+
+        // then
     }
 
 }
